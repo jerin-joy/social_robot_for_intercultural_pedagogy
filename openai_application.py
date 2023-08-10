3e5f6b4594dde34032771ba6d4109a71c949367a
jerin-joy <jerinmemana@gmail.com>
2023-08-10 10:03:46 +0200

    implement basic use of openAI API

--- openai_application.py
+++ openai_application.py
@@ -4,16 +4,21 @@ import openai
 # Set up your OpenAI API credentials
 openai.api_key = api_key
 
+def extract_country(sentence):
+    prompt = f"Extract the name of the country from the following sentence: {sentence}\nCountry: "
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role":"user", "content":prompt}
+        ],
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    country = response['choices'][0]['message']['content']
+    return country
 
-response = openai.Completion.create(
-  model="text-curie-001",
-  prompt = input("You: "),
-  temperature=1,
-  max_tokens=120,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-
-answer = response.choices[0].text.strip()
-print("Nao: ", answer)
+sentence = input("Where do you come from? ")
+country = extract_country(sentence)
+print(f"The country in the sentence '{sentence}' is: {country}")
