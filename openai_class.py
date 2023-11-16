24b8a3112c373055ff62d7dd5f095a9442aac3ab
jerin-joy <jerinmemana@gmail.com>
2023-11-16 14:26:04 +0100

    retry the request for few times in the case of errors

--- openai_class.py
+++ openai_class.py
@@ -1,24 +1,33 @@
 from openai_key import api_key
 import openai
+import time
+import requests
 
 class InformationExtractor:
     def __init__(self):
         # Set up your OpenAI API credentials
         openai.api_key = api_key
 
-    def extract_information(self, text: str, prompt: str, temperature = 0.5) -> str:
-        response = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages = [
-                {"role":"user", "content":prompt}
-            ],
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=temperature,
-        )
-        information = response['choices'][0]['message']['content']
-        return information
+    def extract_information(self, text: str, prompt: str, temperature = 0.5, max_retries=5, timeout=5) -> str:
+        for i in range(max_retries):
+            try:
+                response = openai.ChatCompletion.create(
+                    model = "gpt-3.5-turbo",
+                    messages = [
+                        {"role":"user", "content":prompt}
+                    ],
+                    max_tokens=1024,
+                    n=1,
+                    stop=None,
+                    temperature=temperature,
+                    timeout=timeout  # Add a timeout
+                )
+                information = response['choices'][0]['message']['content']
+                return information
+            except (openai.error.OpenAIError, requests.exceptions.Timeout):
+                print(f"Request failed or timed out, retrying ({i+1}/{max_retries})")
+                time.sleep(1)  # Wait for 1 second before retrying
+        raise Exception("Request failed or timed out after multiple attempts")
 
 
 
